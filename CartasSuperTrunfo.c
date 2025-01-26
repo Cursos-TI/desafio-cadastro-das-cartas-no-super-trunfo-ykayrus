80914357cb75544d15074c5c967efd0b26d703c8
ykayrus <164512943+ykayrus@users.noreply.github.com>
2025-01-26 00:34:51 +0000

    Codigo pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,9 +17,40 @@
 
 #include <stdio.h>
 
-
+// Funçao do código
 int main() {
-    
+    char codigo[4]; // variável do tipo array para armazenar código da cidade.
+    int populacao;  // variável do tipo inteiro para armazenar a população.
+    float area;     // variável do tipo ponto flutuante para armazenar area da cidade.
+    float pib;      // variável do tipo ponto flutuante para armazenar PIB da cidade.
+    int pontos_turisticos; // variável do tipo interio para armazenar os pontos turísticos.
 
-    return 0;
+    // Solicitações de entradas do código
+    printf("Digite o código da carta (Exemplo:B03,A02,etc..): ");
+    scanf("%s", codigo); // // Lê uma string sem espaços e armazena no array 'codigo'
+
+    printf("Digite a população da cidade (digite sem utilizar pontos para separar milhares): ");
+    scanf("%d", &populacao); //Lê um número inteiro e armazena na variável 'populacao'.
+    
+    printf("Digite a área da cidade (km²): ");
+    scanf("%f", &area); // Lê um número de ponto flutuante e armazena na variável 'area'.
+
+    printf("Digite o Pib da cidade (R$ Bilhões): ");
+    scanf("%f", &pib); // Lê um número de ponto flutuante e armazena na variável 'pib'.
+
+    printf("Digite o numero de pontos turísticos da cidade: ");
+    scanf("%d", &pontos_turisticos); // Lê um número inteiro e armazena na variável 'pontos_turisticos'.
+
+    // Exibição dos dados do código
+    printf("\n######### DADOS DA SUA CARTA ########### \n");
+    printf("\nCódigo da carta: %s\n", codigo); // Mostra o código da carta.
+    printf("População: %d \n", populacao);   // Mostra a população da cidade.
+    printf("Área: %f km²\n", area);          // Mostra a área da cidade
+    printf("PIB: R$ %f Bilhões\n", pib);     // Mostra o PIB da cidade
+    printf("Pontos Turísticos: %d\n", pontos_turisticos);  // Mostra o número de pontos turísticos.
+    printf("\n### SUPER TRUNFO - PAÍSES - MATECHECK ###\n");
+
+    return 0; // Indica que o programa terminou com sucesso
+
 }
+
