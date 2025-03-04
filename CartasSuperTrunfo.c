97c9108dadd52456d56b115d45faf8bb8234d953
ykayrus <164512943+ykayrus@users.noreply.github.com>
2025-03-04 14:24:29 -0300

    Update CartasSuperTrunfo.c
    Desafio mestre, adicionado comparadores de atributos.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,53 +1,87 @@
-    #include <stdio.h>
+#include <stdio.h>
 
-    // Funçao do código
-    int main() {
-        
-        char codigo[4]; // variável do tipo array para armazenar código da cidade.
-        int populacao;  // variável do tipo inteiro para armazenar a população.
-        float area;     // variável do tipo ponto flutuante para armazenar area da cidade.
-        float pib;      // variável do tipo ponto flutuante para armazenar PIB da cidade.
-        int pontos_turisticos; // variável do tipo interio para armazenar os pontos turísticos.
-        
-        float densidade_pop; // variável do tipo ponto flutuante para amazenar a densidade populacional.
-        float pib_per_cap;   // variável do tipo ponto flutuante para amazenar o PIB per capita.
-    
-        // Solicitações de entradas do código
-        printf("Digite o código da carta (Exemplo:B03,A02,etc..): ");
-        scanf("%s", codigo); // // Lê uma string sem espaços e armazena no array 'codigo'
-    
-        printf("Digite a população da cidade (digite sem utilizar pontos para separar milhares): ");
-        scanf("%d", &populacao); //Lê um número inteiro e armazena na variável 'populacao'.
-        
-        printf("Digite a área da cidade (km²): ");
-        scanf("%f", &area); // Lê um número de ponto flutuante e armazena na variável 'area'.
-    
-        printf("Digite o Pib da cidade: ");
-        scanf("%f", &pib); // Lê um número de ponto flutuante e armazena na variável 'pib'.
-    
-        printf("Digite o numero de pontos turísticos da cidade: ");
-        scanf("%d", &pontos_turisticos); // Lê um número inteiro e armazena na variável 'pontos_turisticos'.
+int main(){
 
-        // Operadores matemáticos
-        densidade_pop = (float) populacao / area; // Calcula a densidade populacional.
-        pib_per_cap = (float) pib / populacao;    // Calcula o PIB per capita.
+    //Variáveis
+    char codigo_carta1[30], codigo_carta2[30];// variável do tipo array para armazenar código da cidade.
+    unsigned int populacao_carta1, populacao_carta2;
+    float area_carta1, area_carta2;
+    float pib_carta1, pib_carta2;
+    int pontos_turisticos_carta1, pontos_turisticos_carta2;
+    float densidade_populacional_carta1, densidade_populacional_carta2;
+    float pib_per_capita_carta1, pib_per_capita_carta2;
+    float super_poder_carta1, super_poder_carta2;
+
+    //Entrada de dados da carta 1
+    printf("\n### SUPER TRUNFO - PAÍSES - MATECHECK ###\n"); //CABEÇALHO
+    printf(" \n########## CARTA 1 ###########\n\n");//cabeçalho
+    printf("Digite o código da carta 1 (sem espaços): ");
+    scanf("%s", &codigo_carta1);// Lê uma string sem espaços e armazena no array 'codigo da carta1'
+    printf("Digite a população da carta 1: ");
+    scanf("%u", &populacao_carta1);
+    printf("Digite a area da carta 1: ");
+    scanf("%f", &area_carta1);
+    printf("Digite o PIB da carta 1: ");
+    scanf("%f", &pib_carta1);
+    printf("Digite o numero de pontos turísticos da carta 1: ");
+    scanf("%d", &pontos_turisticos_carta1);
+
+    //Entrada de dados da carta 2
+    printf(" \n########## CARTA 2 ###########\n\n");//cabeçalho
+    printf("Digite o código da carta 2 (sem espaços): ");
+    scanf("%s", &codigo_carta2);
+    printf("Digite a população da carta 2: ");
+    scanf("%u", &populacao_carta2);
+    printf("Digite a area da carta 2: ");
+    scanf("%f", &area_carta2);
+    printf("Digite o PIB da carta 2: ");
+    scanf("%f", &pib_carta2);
+    printf("Digite o numero de pontos turísticos da carta 2: ");
+    scanf("%d", &pontos_turisticos_carta2);
+
+    //Operadores matemáticos carta 1
+    densidade_populacional_carta1 = (float)populacao_carta1 / area_carta1;
+    pib_per_capita_carta1 = pib_carta1 / populacao_carta1;
+    super_poder_carta1 = populacao_carta1 + area_carta1 + pib_carta1 + pontos_turisticos_carta1 + pib_per_capita_carta1 + (1 / densidade_populacional_carta1);
     
-        // Exibição dos dados do código
-        printf("\n######### DADOS DA SUA CARTA ########### \n"); // Cabeçalho
-        
-        printf("\nCódigo da carta: %s\n", codigo); // Mostra o código da carta.
-        printf("População: %d \n", populacao);   // Mostra a população da cidade.
-        printf("Área: %f km²\n", area);          // Mostra a área da cidade.
-        printf("PIB: R$ %f \n", pib);     // Mostra o PIB da cidade.
-        printf("Pontos Turísticos: %d\n", pontos_turisticos);  // Mostra o número de pontos turísticos.
-        printf("Densidade Populacional: %f \n", densidade_pop); // Mostra o numero da densidade populacional.
-        printf("PIB per capita: %f \n", pib_per_cap); // Mostra o valor PIB per capita por habitante.
-       
-        printf("\n### SUPER TRUNFO - PAÍSES - MATECHECK ###\n"); // rodapé
-        
-        return 0; // Indica que o programa terminou com sucesso
-    
-    }
-    
-    
-    
+    //Operadores matemáticos carta 2
+    densidade_populacional_carta2 = (float)populacao_carta2 / area_carta2;
+    pib_per_capita_carta2 = pib_carta2 / populacao_carta2;
+    super_poder_carta2 = populacao_carta2 + area_carta2 + pib_carta2 + pontos_turisticos_carta2 + pib_per_capita_carta2 + (1 / densidade_populacional_carta2);
+   
+    //Exibição de dados carta 01
+    printf("\n######### EXIBIÇÃO CARTA 01 ###########\n\n ");//cabeçalho
+    printf("Código: %s\n", codigo_carta1);// Mostra o código da carta.
+    printf("População: %u\n", populacao_carta1);
+    printf("Área: %f\n", area_carta1);
+    printf("PIB: %f\n", pib_carta1);
+    printf("Pontos turísticos: %d\n", pontos_turisticos_carta1);
+    printf("Densidade populacional: %f\n", densidade_populacional_carta1);
+    printf("PIB per capita: %f\n", pib_per_capita_carta1);
+    printf("Super Poder: %f\n", super_poder_carta1);
+
+    //Exibição de dados carta 02
+    printf("\n######### EXIBIÇÃO CARTA 02 ###########\n\n ");//cabeçalho
+    printf("Código: %s\n", codigo_carta2);
+    printf("População: %u\n", populacao_carta2);
+    printf("Área: %f\n", area_carta2);
+    printf("PIB: %f\n", pib_carta2);
+    printf("Pontos turísticos: %d\n", pontos_turisticos_carta2);
+    printf("Densidade populacional: %f\n", densidade_populacional_carta2);
+    printf("PIB per capita: %f\n", pib_per_capita_carta2);
+    printf("Super Poder: %f\n", super_poder_carta2);
+
+    //Comparação de atributos entre as cartas
+    printf("\n## Resultado carta1 =(1) carta 2 =(0) #### ESTÁCIO FATALITY ####\n\n"); //cabeçalho
+    printf("População: Carta %d venceu\n", populacao_carta1 > populacao_carta2);
+    printf("Área: Carta %d venceu\n", area_carta1 > area_carta2);//Exibe e compara se area da carta 1 é maior que área da carta 2
+    printf("PIB: Carta %d venceu\n",  pib_carta1 > pib_carta2);
+    printf("Pontos turísticos: Carta %d venceu\n",  pontos_turisticos_carta1 > pontos_turisticos_carta2);
+    printf("densidade populacional: Carta %d venceu\n", densidade_populacional_carta1 > densidade_populacional_carta2);
+    printf("PIB per capita: Carta %d venceu\n", pib_per_capita_carta1 > pib_per_capita_carta2);
+    printf("Super Poder: Carta %d venceu\n", super_poder_carta1 > super_poder_carta2);
+    printf("\n### SUPER TRUNFO - PAÍSES - MATECHECK ###\n"); // rodapé
+
+    return 0;
+
+}
